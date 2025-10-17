5a347843b1a44c5ac37dcb7ae7762c1fa12f76d5
Momchil Paskov <paskovmomchil@gmail.com>
2025-10-17 22:44:11 +0300

    adding animation

--- function.js
+++ function.js
@@ -14,3 +14,21 @@ window.onclick = function(event) {
     }
   }
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll('.reveal');
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 0; // how early it triggers
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
